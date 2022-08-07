e5ded091ea604f6a66ea6b7dafe626a306df0e60
Raghavendra M J <raghavendramj123@gmail.com>
2022-08-07 13:29:55 +0530

    Object creation - employee creation

--- JavaScript/basics/12_Objects.js
+++ JavaScript/basics/12_Objects.js
@@ -1,31 +1,48 @@
 var carObject = {
-  name: "Fiat",
-  model: "Punto",
-  weight: "1200kg",
-  color: "white",
-  start: function () {
-    console.log("Car started!");
-  },
-  stop: function () {
-    console.log("Car stopped!");
-  },
-  drive: function () {
-    console.log("Now the car is been driven :- ", this);
-  },
+    name: "Fiat",
+    model: "Punto",
+    weight: "1200kg",
+    color: "white",
+    start: function () {
+        console.log("Car started!");
+    },
+    stop: function () {
+        console.log("Car stopped!");
+    },
+    drive: function () {
+        console.log("Now the car is been driven :- ", this);
+    },
 };
 
 function objectExample() {
-  console.log("carObject :- ", carObject);
-  console.log("carObject.name :- ", carObject.name);
-  console.log("carObject['model'] :- ", carObject["model"]);
-  carObject.drive();
+    console.log("carObject :- ", carObject);
+    console.log("carObject.name :- ", carObject.name);
+    console.log("carObject['model'] :- ", carObject["model"]);
+    carObject.drive();
 }
 
 // objectExample();
 
 function forInLoop() {
-    for(var eachProperty in carObject){
-        console.log("For in :- Key is ", eachProperty, " value :- ", carObject[eachProperty]);
+    for (var eachProperty in carObject) {
+        console.log(
+            "For in :- Key is ",
+            eachProperty,
+            " value :- ",
+            carObject[eachProperty]
+        );
     }
 }
-forInLoop();
+// forInLoop();
+
+function question() {
+    var music = {
+        rock: "guitar",
+        classical: "violin",
+    };
+    var classical = "rock";
+    console.log("music.classical :- ", music.classical);
+    console.log("music[classical] :- ", music[classical]);
+}
+
+question();
---
+++ JavaScript/basics/13_ObjectCreation.js
@@ -0,0 +1,51 @@
+function firstWay() {
+  // 1st Way -> Factory function
+  function createCircle(raduis) {
+    return {
+      raduis,
+      draw: function () {
+        console.log("Factory fuction- DRAW", this.raduis);
+      },
+    };
+  }
+  var facCricle1 = createCircle(10);
+  console.log(facCricle1);
+  var facCricle2 = createCircle(23);
+  console.log(facCricle2);
+}
+
+function secondWay() {
+  //2nd Way -> Constructor function
+  function Cricle(radius) {
+    this.radius = radius;
+    this.draw = function () {
+      console.log("Circle -> Constructor function - DRAW ", this.radius);
+    };
+  }
+  var circle1 = new Cricle(45);
+  console.log(circle1);
+  circle1.draw();
+
+  // 1. new -> create an empty object {}
+  // 2. it will set that empty object to that current object
+  // 3. returns the object from the fuction
+  // Happens in sequence and automatically
+
+  function Employee(id, fname, lname, city, email) {
+    this.id = id;
+    this.fname = fname;
+    this.lname = lname;
+    this.city = city;
+    this.fullName = function () {
+      return this.fname + " " + this.lname;
+    };
+  }
+
+  var employee1 = new Employee(1, "raghav", "mj", "bangalore", "test@gmail.com");
+  var employee2 = new Employee(2, "keshav", "anand", "madikeri");
+  console.log("employee1 -> ", employee1);
+  console.log("employee1.fullName() -> ", employee1.fullName());
+  console.log("employee2 -> ", employee2);
+  console.log("employee2.fullName() -> ", employee2.fullName());
+}
+secondWay();
