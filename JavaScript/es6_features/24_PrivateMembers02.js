6045ea7962e49adde7da717312eefb12a1aa49da
Raghavendra M J <raghavendramj123@gmail.com>
2022-09-15 22:40:25 +0530

    Export and import!

--- JavaScript/es6_features/24_PrivateMembers02.js
+++ JavaScript/es6_features/24_PrivateMembers02.js
@@ -1,22 +1,22 @@
-class Person {
-    #firstName;
-    #lastName;
-    constructor(firstName, lastName) {
-      this.#firstName = firstName;
-      this.#lastName = lastName;
-    }
-    //Deafult parameter = true
-    getFullName(format = true) {
-      return format ? this.#firstLast() : this.#lastFirst();
-    }
-  
-    #firstLast() {
-      return `${this.#firstName} ${this.#lastName}`;
-    }
-    #lastFirst() {
-      return `${this.#lastName}, ${this.#firstName}`;
-    }
+export class Person {
+  #firstName;
+  #lastName;
+  constructor(firstName, lastName) {
+    this.#firstName = firstName;
+    this.#lastName = lastName;
   }
-  
-  let person = new Person('John', 'Doe');
-  console.log(person.getFullName());
+  //Deafult parameter = true
+  getFullName(format = true) {
+    return format ? this.#firstLast() : this.#lastFirst();
+  }
+
+  #firstLast() {
+    return `${this.#firstName} ${this.#lastName}`;
+  }
+  #lastFirst() {
+    return `${this.#lastName}, ${this.#firstName}`;
+  }
+}
+
+// let person = new Person("Ricky", "Manas");
+// console.log(person.getFullName());
---
+++ JavaScript/es6_features/26_ImportClassAndTest.js
@@ -0,0 +1,7 @@
+import { Person } from "./24_PrivateMembers02.js";
+
+let person = new Person("John", "Doe");
+console.log(person.getFullName());
+
+let person2 = new Person("Raghav", "MJ"); 
+console.log(person2.getFullName());
