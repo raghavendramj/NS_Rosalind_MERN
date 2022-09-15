7a467b38ddd488ec90bb05515a39caa3101c680c
Raghavendra M J <raghavendramj123@gmail.com>
2022-09-15 22:24:35 +0530

    Private members

--- JavaScript/es6_features/23_PrivateMembers.js
+++ JavaScript/es6_features/23_PrivateMembers.js
@@ -2,14 +2,19 @@ class Person {
   #age;
   constructor(name, age) {
     this.name = name;
-    this.#age = age; //Private field '#age' must be declared in an enclosing class
+    this.#age = age; //Private field '#age'
   }
 
   greet() {
+    this.#printContent();
+  }
+
+  //Private Function
+  #printContent() {
     console.log(`name: ${this.name}, age: ${this.#age}`);
   }
 }
 
 let joe = new Person("Joe", 27);
 joe.greet();
-console.log("Name:- ", joe); //You can't access
+//joe.printContent(); //TypeError: joe.printContent is not a function
---
+++ JavaScript/es6_features/25_getterSetterPrivate.js
@@ -0,0 +1,9 @@
+class MyClass {
+  #field;
+  get #myField() {
+    return this.#field;
+  }
+  set #myField(value) {
+    this.#field = value;
+  }
+}
