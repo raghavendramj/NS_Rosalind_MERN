130555e6b409c3365aeb45e87d713f3eb203d81f
Raghavendra M J <raghavendramj123@gmail.com>
2022-10-15 22:33:55 +0530

    Added react project!

---
+++ ReactJS/newton-react-app/src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Product from "./product";
+
+function App() {
+  return <Product />;
+}
+
+export default App;
---
+++ ReactJS/newton-react-app/src/product.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Product extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Newton School</h1>
+                <h2>Edtech platform</h2>
+                <p>Place to learn and make carrer out of it</p>
+            </div>
+        );
+    }
+}
+
+export default Product;
